083985941362183f4eae447bbbdbbfc650a861ae
lysiuk03 <lysiuk03@gmail.com>
2024-05-06 21:51:58 +0300

    Commented

--- app/src/main/java/com/example/shop/MainActivity.java
+++ app/src/main/java/com/example/shop/MainActivity.java
@@ -15,17 +15,16 @@ import com.example.shop.application.HomeApplication;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        //знаходимо елемент на сторінці
-        ImageView ivAvatar = findViewById(R.id.ivAvatar);
-        String url = "http://10.0.2.2:5297/images/1.jpg";
-        Glide.with(HomeApplication.getAppContext())
-                .load(url)
-                .apply(new RequestOptions().override(400))
-                .into(ivAvatar);
-    }
+    // @Override
+    // protected void onCreate(Bundle savedInstanceState) {
+    //    super.onCreate(savedInstanceState);
+    //     setContentView(R.layout.activity_main);
+    //     //знаходимо елемент на сторінці
+    //    ImageView ivAvatar = findViewById(R.id.ivAvatar);
+    //    String url = "http://10.0.2.2:5297/images/1.jpg";
+    //    Glide.with(HomeApplication.getAppContext())
+    //            .load(url)
+    //            .apply(new RequestOptions().override(400))
+    //            .into(ivAvatar);
+    //}
 }
